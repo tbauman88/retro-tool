a1671af49dac40e29ea15e72113a60e01b4871c1
Tyler Bauman <tyler.bauman@icloud.com>
2026-01-22 16:00:46 -0500

    replace cypress.json with cypress.config.ts

---
+++ apps/client-e2e/cypress.config.ts
@@ -0,0 +1,19 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+    }),
+    fileServerFolder: '.',
+    fixturesFolder: './src/fixtures',
+    specPattern: './src/integration/**/*.spec.ts',
+    supportFile: './src/support/index.ts',
+    video: true,
+    videosFolder: '../../dist/cypress/apps/client-e2e/videos',
+    screenshotsFolder: '../../dist/cypress/apps/client-e2e/screenshots',
+    chromeWebSecurity: false,
+    modifyObstructiveCode: false,
+  },
+});
