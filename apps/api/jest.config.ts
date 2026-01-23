66561218a34d3c260630ddb5d20fb31480c94317
Tyler Bauman <tyler.bauman@icloud.com>
2026-01-23 13:41:49 -0500

    chore: update jest configuration to include coverage reporters

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,15 +1,14 @@
 /* eslint-disable */
-const path = require('path');
-
-export default {
+module.exports = {
   displayName: 'api',
   preset: '../../jest.preset.js',
   clearMocks: true,
-  testEnvironment: path.join(__dirname, './prisma-test-environment.ts'),
+  testEnvironment: require('path').join(__dirname, './prisma-test-environment.ts'),
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   setupFilesAfterEnv: ['./jest.setup.ts'],
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/api',
+  coverageReporters: ['lcov', 'text'],
 };
